63ae331c978c9284d3eb44c086964f67bdd5f4a6
Holger Sindbaek <holgersindbaek@gmail.com>
2024-10-09 22:13:30 +0200

    Add logs

--- scripts/eas-build-post-install-script.js
+++ scripts/eas-build-post-install-script.js
@@ -13,6 +13,37 @@ if (process.platform !== "darwin")
 else {
   console.log("RUNNING eas-build-post-install-script!");
 
+  // Log environment variables
+  console.log("Environment variables:", JSON.stringify(process.env, null, 2));
+
+  // Log contents of directories
+  console.log("Contents of current directory:", fs.readdirSync(process.cwd()));
+  console.log("Contents of ios directory:", fs.readdirSync(path.join(process.cwd(), 'ios')));
+
+  // Log full path of this script
+  console.log("Full path of this script:", __filename);
+
+  // Log Node.js version
+  console.log("Node.js version:", process.version);
+
+  // Log Xcode version
+  exec('xcodebuild -version', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`Xcode version: ${stdout}`);
+  });
+
+  // Log Ruby version
+  exec('ruby -v', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`Ruby version: ${stdout}`);
+  });
+
   const sourcePath = "./plugin/src";
 
   const scriptFileName = "AppLovinQualityServiceSetup-ios.rb";
@@ -21,12 +52,29 @@ else {
   const newPath = path.join(process.cwd(), "ios", scriptFileName);
   fs.copyFileSync(path.join(dir, sourcePath, scriptFileName), newPath);
 
-  const exec = require("child_process").exec;
+  console.log("Current working directory:", process.cwd());
+  console.log("New path for Ruby script:", newPath);
+  console.log("File exists:", fs.existsSync(newPath));
 
-  const child = exec(`ruby ${newPath}`);
+  // Log contents of the Podfile
+  const podfilePath = path.join(process.cwd(), 'ios', 'Podfile');
+  if (fs.existsSync(podfilePath)) {
+    console.log("Contents of Podfile:", fs.readFileSync(podfilePath, 'utf8'));
+  } else {
+    console.log("Podfile does not exist at path:", podfilePath);
+  }
+
+  const command = `ruby "${newPath}"`;
+  console.log("Executing command:", command);
+
+  const child = exec(command);
 
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
 
   function callback(e) {
     fs.unlinkSync(newPath);
@@ -36,5 +84,8 @@ else {
 
   child.on("exit", callback);
   child.on("close", callback);
-  child.on("error", callback);
+  child.on("error", (error) => {
+    console.error('Error executing Ruby script:', error);
+    callback(error);
+  });
 }
