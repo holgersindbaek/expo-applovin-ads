4c0d69c3005483fbd35b51cc89a03c0997e02d13
Holger Sindbaek <holgersindbaek@gmail.com>
2024-10-10 11:57:00 +0200

    Fix bug

--- plugin/src/withDangerousMod.js
+++ plugin/src/withDangerousMod.js
@@ -2,7 +2,45 @@ const { withDangerousMod, withXcodeProject } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
-function addRubyScriptToXcodeProject(config) {
+function modifyPodfile(contents) {
+  // Add AppLovin SDK and mediation pods to the Podfile
+  const appLovinPods = `
+  pod 'AppLovinSDK'
+  pod 'AppLovinMediationGoogleAdapter'
+  pod 'AppLovinMediationUnityAdsAdapter'
+  pod 'AppLovinMediationVungleAdapter'
+  pod 'AppLovinMediationInMobiAdapter'
+`
+
+  const appLovinSetup = `
+post_install do |installer|
+  system("ruby \#{Dir.pwd}/AppLovinQualityServiceSetup-ios.rb")
+end
+`
+
+  // Replace the last 'end' in the Podfile with the AppLovin pods and setup
+  return contents.replace(/end[\n ]*?$/g, `${appLovinPods}\n${appLovinSetup}\nend`)
+}
+
+module.exports = function withAppLovinPodfile(config) {
+  // First, modify the Podfile
+  config = withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const filePath = path.join(
+        config.modRequest.platformProjectRoot,
+        'Podfile',
+      )
+
+      let contents = fs.readFileSync(filePath, 'utf-8')
+      contents = modifyPodfile(contents)
+      fs.writeFileSync(filePath, contents)
+
+      return config
+    },
+  ])
+
+  // Then, modify the Xcode project
   return withXcodeProject(config, (config) => {
     const xcodeProject = config.modResults;
     const projectRoot = config.modRequest.projectRoot;
@@ -12,43 +50,4 @@ function addRubyScriptToXcodeProject(config) {
     
     return config;
   });
-}
-
-module.exports = function withAppLovinPodfile(config) {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const filePath = path.join(
-        config.modRequest.platformProjectRoot,
-        'Podfile',
-      )
-
-      let contents = fs.readFileSync(filePath, 'utf-8')
-
-      // Add AppLovin SDK and mediation pods to the Podfile
-      const appLovinPods = `
-  pod 'AppLovinSDK'
-  pod 'AppLovinMediationGoogleAdapter'
-  pod 'AppLovinMediationUnityAdsAdapter'
-  pod 'AppLovinMediationVungleAdapter'
-  pod 'AppLovinMediationInMobiAdapter'
-`
-
-      const appLovinSetup = `
-post_install do |installer|
-  system("ruby \#{Dir.pwd}/AppLovinQualityServiceSetup-ios.rb")
-end
-`
-
-      // Replace the last 'end' in the Podfile with the AppLovin pods and setup
-      contents = contents.replace(/end[\n ]*?$/g, `${appLovinPods}\n${appLovinSetup}\nend`)
-
-      fs.writeFileSync(filePath, contents)
-
-      // Add Ruby script to Xcode project
-      config = addRubyScriptToXcodeProject(config);
-
-      return config
-    },
-  ])
 }
